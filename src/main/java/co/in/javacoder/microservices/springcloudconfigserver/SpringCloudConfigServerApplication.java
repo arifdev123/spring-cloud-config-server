5aa03263b86fb50b1f5e51bd3a6a95be7207639f
Mohamed Ismail Arif <mhdarif.vdm@gmail.com>
2020-03-12 13:04:10 +0530

    upto zuul api gateway implementation done

---
+++ src/main/java/co/in/javacoder/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package co.in.javacoder.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer // very important for a config server
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
